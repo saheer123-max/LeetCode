352cfb6f5a2cfaf32545ac2c012731deb406a491
saheer123-max <saheeraslam39@gmail.com>
2025-04-03 11:09:11 +0530

    Time: 30 ms (34.22%), Space: 48.3 MB (74.02%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,12 @@
+class Solution {  // Ensure 'Solution' class is defined
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { };
+    }
+}
